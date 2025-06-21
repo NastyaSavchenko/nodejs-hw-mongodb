b93629b75656a376b3d0faa7368df90e8778470c
Анастасія Савченко <savyle4ka13@gmail.com>
2025-06-21 02:04:32 +0100

    add api docs route for swagger snd gix update and create contact (deleted userId)

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
+import * as fs from 'node:fs';
 import express from 'express';
 import path from 'node:path';
 import cors from 'cors';
 import pino from 'pino';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
+const SWAGGER_DOCS = JSON.parse(
+  fs.readFileSync(path.join('docs', 'swagger.json'), 'utf-8'),
+);
+
 const app = express();
 const logger = pino();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(SWAGGER_DOCS));
 app.use('/photo', express.static(path.resolve('src', 'uploads', 'photos')));
 app.use(cors());
 app.use(cookieParser());
